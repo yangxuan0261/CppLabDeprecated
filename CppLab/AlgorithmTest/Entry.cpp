a3474f07d299d5b6c909a34106b78a66c5b94656
杨旋 <yx0588@175game.com>
2017-03-27 14:06:21 +0800

    s

--- CppLab/AlgorithmTest/Entry.cpp
+++ CppLab/AlgorithmTest/Entry.cpp
@@ -76,12 +76,39 @@ namespace AT1 {
 		printVecInt(findDuplicates2(srcVec));
 	}
 
+	//------------------------------ 
+	string longestPalindrome(string s) {
+		if (s.empty()) return "";
+		if (s.size() == 1) return s;
+		int min_start = 0, max_len = 1;
+		for (int i = 0; i < s.size();) {
+			if (s.size() - i <= max_len / 2) break;
+			int j = i, k = i;
+			while (k < s.size() - 1 && s[k + 1] == s[k]) ++k; // Skip duplicate characters.
+			i = k + 1;
+			while (k < s.size() - 1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+			int new_len = k - j + 1;
+			if (new_len > max_len) { min_start = j; max_len = new_len; }
+		}
+		return s.substr(min_start, max_len);
+	}
+
+	//https://discuss.leetcode.com/topic/12187/simple-c-solution-8ms-13-lines
+	//查找最长的回文子字符串
+	void test3()
+	{
+		string src = "bababd";
+		string str = longestPalindrome(src);
+		printf("%s\n", str.c_str());
+	}
+
 };
 
 int main()
 {
 	//AT1::test1();
-	AT1::test2();
+	//AT1::test2();
+	AT1::test3();
 
 	system("pause");
 	return 0;
